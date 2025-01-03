c5f36ab9778a2970fb5d8e0fa6d2ad949f76c044
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-03 16:18:01 -0300

    adicionando cadastro de comanda para clientes

--- src/pages/clientMenu/registerClient.jsx
+++ src/pages/clientMenu/registerClient.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { NewCheck } from "../../components/newCheck";
+import { useToggleView } from "../../contexts/ToggleViewNote";
+
 export const RegisterClient = () => {
+    
+    const navigate = useNavigate();
+
+    const { setToggleView } = useToggleView();
+
+    useEffect(() => {
+        const check_id = localStorage.getItem("check_id");
+        if (check_id) {
+            navigate(`/garcom/comanda/${check_id}`);
+        };
+
+        setToggleView(true);
+    }, []);
+
     return (
-        <div>Register Client</div>
+        <NewCheck is_client={true} />
     );
 };
