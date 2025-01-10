22da887622c9d0ce4ef08424242c7611c3984890
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-10 17:28:50 -0300

    melhorando comunicação com o back

--- src/service/axiosConfig/index.js
+++ src/service/axiosConfig/index.js
@@ -4,8 +4,23 @@ const API = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL_BACK,
     withCredentials: true,
     headers: {
-        "Content-Type": "application/json", 
+        "Content-Type": "application/json",
     },
+});
+
+API.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    };
+    return config;
+}, (error) => {
+    if (error.response?.status === 401) {
+        sessionStorage.removeItem("token");
+        localStorage.removeItem("func");
+        window.location.href = "/login";
+    };
+    return Promise.reject(error);
 });
 
 export { API };
