dd3a2e40699d6b439395c31363aa4658b62f2a22
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-10 17:28:35 -0300

    removendo token e função

--- src/service/logout/LogoutService.js
+++ src/service/logout/LogoutService.js
@@ -1,12 +1,6 @@
-import { API } from "../axiosConfig";
-
-const logout = async () => {
-
-    try {
-        await API.get("/api/logout");
-    } catch (error) {
-        return new Error(error.message);
-    };
+const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("func");
 };
 
 export const LogoutService = {
