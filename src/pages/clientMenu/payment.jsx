78e88e99b55e1db753019d5e020b35c4b6f5770c
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-11 13:26:59 -0300

    Iniciando configuração do mercado pago

--- src/pages/clientMenu/payment.jsx
+++ src/pages/clientMenu/payment.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { Navbar } from "../../components/navbar";
+
 export const Payment = () => {
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [total_value, setTotalValue] = useState(0);
+
+    useEffect(() => {
+        const get_total_value = localStorage.getItem("total_value");
+
+        if (get_total_value) {
+            setTotalValue(get_total_value);
+        }
+    }, []);
+
     return (
-        <div>Payment</div>
+        <>
+            <Navbar title="Pagamento" />
+            <div className="payment">
+                <p>Total: R$ {total_value}</p>
+            </div>
+        </>
     );
 };
