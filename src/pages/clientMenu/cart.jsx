0df8d5e477c27c40a662c1b04406c4eee1fb5e96
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-12 01:41:01 -0300

    Iniciando implementação com a api do mercado pago

--- src/pages/clientMenu/cart.jsx
+++ src/pages/clientMenu/cart.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 import { Navbar } from "../../components/navbar";
 
+import { CheckService } from "../../service/check/CheckService";
 import { ProductService } from "../../service/product/ProductService";
+import { PaymentService } from "../../service/payment/PaymentService";
 
 export const Cart = () => {
+
     const { id } = useParams();
     const navigate = useNavigate();
 
@@ -13,6 +17,8 @@ export const Cart = () => {
     const [productsSelected, setProductsSelected] = useState([]);
     const [productsInCart, setProductsInCart] = useState([]);
     const [total_value, setTotalValue] = useState(0);
+    const [client, setClient] = useState("");
+    const [email, setEmail] = useState("");
 
     const getProducts = useCallback(async () => {
         try {
@@ -23,12 +29,23 @@ export const Cart = () => {
         };
     }, []);
 
+    const getCheck = async () => {
+        CheckService.getById(id)
+            .then(response => {
+                setClient(response.name_client);
+            })
+            .catch(error => {
+                toast.error(error.message);
+            });
+    };
+
     useEffect(() => {
         const selectedProducts = localStorage.getItem("selected_product");
         if (selectedProducts) {
             setProductsSelected(JSON.parse(selectedProducts));
         };
 
+        getCheck();
         getProducts();
     }, [getProducts]);
 
@@ -64,45 +81,107 @@ export const Cart = () => {
         };
     }, [products, productsSelected]);
 
+    const payment = useCallback(() => {
+        const paymentData = {
+            transaction_amount: total_value,
+            description: `Pagamento da comanda ${client}`,
+            payment_method_id: "pix",
+            idempotencyKey: client,
+            payer: {
+                email: email || "no-email@teste.com"
+            },
+            back_urls: {
+                success: `http://localhost:3000/${id}/proof`,
+                failure: `http://localhost:3000/${id}/payment_failure`,
+                pending: `http://localhost:3000/${id}/payment_failure`
+            },
+            auto_return: "all",
+            items: productsInCart.map((product) => (
+                {
+                    title: product.product_name,
+                    description: product.obs,
+                    quantity: product.quantity,
+                    unit_price: product.price,
+                    currency: "BRL",
+                }
+            ))
+        };
+
+        // TODO: Tentar utilizar o Pix como forma de pagamento
+        PaymentService.createPayment(paymentData)
+            .then(response => {
+                console.log(response);
+                // window.location.href = response
+            })
+            .catch(error => {
+                console.error(error);
+                toast.error("Erro ao processar pagamento");
+            });
+    }, [total_value, client]);
+
     return (
         <>
             <Navbar title="Meu carrinho" />
-            <div className="px-10 py-14 shadow-xl bg-[#D39825]/10">
-                <table className="max-w-2/3 flex gap-5 flex-col divide-y divide-dashed divide-slate-700">
-                    <tr className="flex justify-between items-center">
-                        <th>Und.</th>
-                        <th>Produto</th>
-                        <th>Preço</th>
-                    </tr>
-                    {productsInCart.map((product, index) => (
-                        <div className="">
-                            <tr key={index} className="flex justify-between gap-1 text-slate-700 font-semibold">
-                                <td className="flex items-center justify-between gap-2">
-                                    <span className="text-[#EB8F00]">{product.quantity}x</span>
-                                </td>
-
-                                <td>
-                                    <span>{product.product_name}</span>
-                                </td>
-
-                                <td>
-                                    <span className="font-bold text-slate-500">R$ {product.price.toFixed(2).replace(".", ",")}</span>
-                                </td>
+            <div className="flex flex-col items-center gap-10">
+                <div className="px-10 py-14 shadow-xl bg-[#D39825]/10">
+                    <Toaster />
+                    <h1 className="text-center text-slate-900 font-bold text-[32px]">{client}</h1>
+                    <table className="max-w-2/3 flex gap-5 flex-col divide-y divide-dashed divide-slate-700">
+                        <thead>
+                            <tr className="flex justify-between items-center">
+                                <th>Und.</th>
+                                <th>Produto</th>
+                                <th>Preço</th>
                             </tr>
-                            {product.obs && (
-                                <p className="text-[#EB8F00]">OBS: <span className="text-slate-500">{product.obs}</span></p>
-                            )}
-                        </div>
-                    ))}
-                </table>
-                <h2 className="mt-5 text-center text-slate-900 font-bold text-[28px]">
-                    Total: <span className="text-slate-500">R$ {parseFloat(total_value).toFixed(2).replace(".", ",")}</span>
-                </h2>
-            </div>
-            <div className="fixed bottom-0 w-full flex flex-col justify-between items-center px-5 py-3 bg-[#EB8F00] text-slate-100">
-                <button className="w-2/3 px-1 py-2 text-[20px] font-bold rounded-xl bg-[#1C1D26] hover:bg-[#EB8F00] hover:text-[#1C1D26] border-2 border-transparent hover:border-[#1C1D26] transition-all delay-75"
-                    onClick={() => navigate(`/${id}/payment`)}
-                >Pagar</button>
+                        </thead>
+                        {productsInCart.map((product, index) => (
+                            <tbody key={index}>
+                                <tr className="flex justify-between gap-1 text-slate-700 font-semibold">
+                                    <td className="flex items-center justify-between gap-2">
+                                        <span className="text-[#EB8F00]">{product.quantity}x</span>
+                                    </td>
+
+                                    <td>
+                                        <span>{product.product_name}</span>
+                                    </td>
+
+                                    <td>
+                                        <span className="font-bold text-slate-500">R$ {product.price.toFixed(2).replace(".", ",")}</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        {product.obs && (
+                                            <p className="text-[#EB8F00]">OBS: <span className="text-slate-500">{product.obs}</span></p>
+                                        )}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        ))}
+                    </table>
+                    <h2 className="mt-5 text-center text-slate-900 font-bold text-[28px]">
+                        Total: <span className="text-slate-500">R$ {parseFloat(total_value).toFixed(2).replace(".", ",")}</span>
+                    </h2>
+
+                </div>
+                <div>
+                    <label>
+                        <input
+                            type="email"
+                            className="w-[250px] border rounded-xl p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Digite seu e-mail"
+                            onChange={(event) => setEmail(event.target.value)}
+                            value={email}
+                            required
+                        />
+                    </label>
+                </div>
+
+                <div className="fixed bottom-0 w-full flex flex-col justify-between items-center px-5 py-3 bg-[#EB8F00] text-slate-100">
+                    <button className="w-2/3 px-1 py-2 text-[20px] font-bold rounded-xl bg-[#1C1D26] hover:bg-[#EB8F00] hover:text-[#1C1D26] border-2 border-transparent hover:border-[#1C1D26] transition-all delay-75"
+                        onClick={() => payment()}
+                    >Pagar</button>
+                </div>
             </div>
         </>
     );
