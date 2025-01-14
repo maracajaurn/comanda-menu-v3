2d3c33115deb0d2bbff57ada00636104b9cb6dc3
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-13 22:04:19 -0300

    serviço para controlar pagamentos

--- src/service/payment/PaymentService.js
+++ src/service/payment/PaymentService.js
@@ -11,6 +11,18 @@ const createPayment = async (paymentData) => {
     };
 };
 
+const createPaymentPix = async (paymentData) => {
+    try {
+        const res = await API.post("/api/payment/process_payment/pix", paymentData);
+        if (res.data) return res.data;
+
+        return new Error(res.message);
+    } catch (error) {
+        return new Error(error.message);
+    };
+};
+
 export const PaymentService = {
     createPayment,
+    createPaymentPix
 };
