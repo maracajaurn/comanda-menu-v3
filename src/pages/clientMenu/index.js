32c7680a72efb9630b3385a2f1fe2370eb25171d
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-17 12:28:48 -0300

    criando arquivo de exportação

---
+++ src/pages/clientMenu/index.js
@@ -0,0 +1,6 @@
+export { Cart } from "./cart";
+export { Menu } from "./menu";
+export { PaymentFailure } from "./paymentFailure";
+export { Proof } from "./proof";
+export { RegisterClient } from "./registerClient";
+export { WaitForProducts } from "./waitForProducts";
