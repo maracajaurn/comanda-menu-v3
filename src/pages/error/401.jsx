a24801530ae7b1536b3217d55c84e9297123d04c
JackSSads <contatojacksonsouza@hotmail.com>
2025-01-17 12:28:57 -0300

    Página de erro

---
+++ src/pages/error/401.jsx
@@ -0,0 +1,41 @@
+import { useLoader } from "../../contexts";
+
+import { Navbar, Footer } from "../../components";
+
+import { XError } from "../../libs/icons";
+
+export const NotAuthorized = () => {
+
+    const { setLoading } = useLoader();
+
+    setLoading(false);
+
+    return (
+        <>
+            <Navbar title="401" url />
+            <div className="w-full flex flex-col px-5 items-center gap-14">
+
+                <div className="px-10 pt-5 pb-14 gap-5 flex flex-col justify-center shadow-xl shadow-slate-400 bg-[#D39825]/10">
+                    <div>
+                        <div className=" flex justify-center">
+                            <p className="h-[50px] w-[50px] rounded-full text-white bg-red-500"><XError size={15} /></p>
+                        </div>
+                        <h1 className="text-center text-slate-900 font-bold text-[30px]">
+                            401 - Não autorizado
+                        </h1>
+                    </div>
+
+                    <div className="flex flex-col gap-5 items-center">
+                        <p className="text-center text-[1.3em]">
+                            Vai pra onde? Você não tem autorização para acessar essa página.
+                        </p>
+                        <span className="text-[3em]">🤨</span>
+                        <p className="text-center">*Caso querira tirar alguma dúvida, comunique com nossos atendentes.</p>
+                    </div>
+                </div>
+            </div>
+
+            <Footer is_client />
+        </>
+    );
+};
