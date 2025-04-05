53a6f00f93f866dd5abdb727011a35c43a0d3fc2
JackSSads <contatojacksonsouza@hotmail.com>
2025-04-05 19:08:28 -0300

    atualizando layout

--- src/pages/clientMenu/menu.jsx
+++ src/pages/clientMenu/menu.jsx
@@ -254,50 +254,56 @@ export const Menu = () => {
                 </div>
 
                 {itensFiltrados.map((item, index) => (
-                    <div key={index} className={`flex flex-col justify-between items-center w-full rounded-xl bg-slate-100/50 shadow-md border`}>
-                        {item.image && (
-                            <div className="h-[300px] w-full rounded-md"
-                                style={{
-                                    backgroundImage: `url(${item.image})`,
-                                    backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
-                                }}>
-                                <div className={`
+                    <div key={index} className={`flex flex-col py-4 px-6 w-full rounded-xl bg-slate-100/50 shadow-md border`}>
+                        <div className="flex items-center justify-between gap-1">
+                            {item.image && (
+                                <div className="h-[120px] w-[120px] rounded-md bg-slate-300"
+                                    style={{
+                                        backgroundImage: `url(${item.image})`,
+                                        backgroundSize: 'cover',
+                                        backgroundPosition: 'center',
+                                    }}>
+                                    <div className={`
                                     ${selectedProduct.findIndex(product => product[1] === item.product_id) === -1 && "hidden"}
                                     w-[30px] h-[30px] flex justify-center items-center bg-green-500 rounded-full relative -top-3 -left-3 z-10`}>
-                                    <h6 className="text-white"><CheckProduct /></h6>
+                                        <h6 className="text-white"><CheckProduct /></h6>
+                                    </div>
                                 </div>
-                            </div>
-                        )}
+                            )}
 
-                        <div className="w-full flex flex-col items-center justify-between gap-2 mt-3">
-                            <h3 className="text-slate-900 text-[25px] font-bold">{item.product_name}</h3>
-                            <p className="text-slate-500 text-[15px] font-semibold">{item.description}</p>
-                            <h3 className="text-slate-500 text-[30px] font-semibold">R$ {item.price.toFixed(2).replace(".", ",")}</h3>
+                            <div className="flex flex-col items-center justify-between gap-2">
+                                <h3 className="text-slate-900 text-[25px] font-bold">{item.product_name}</h3>
+                                <p className="text-slate-500 text-[15px] font-semibold">{item.description}</p>
+                                <h3 className="text-slate-500 text-[30px] font-semibold">R$ {item.price.toFixed(2).replace(".", ",")}</h3>
+                            </div>
                         </div>
 
-                        <label>
-                            <textarea
-                                placeholder="Observação"
-                                className="w-full mt-1 border border-slate-500 rounded-[5px] p-1"
-                                onChange={(e) => obsProduct(item.product_id, e.target.value)}
-                                value={selectedProduct.find(product => product[1] === item.product_id)?.[3] || ""}
-                                disabled={selectedProduct.findIndex(product => product[1] === item.product_id) === -1}
-                            />
-                        </label>
+                        <div className="flex justify-between gap-2 mt-5">
+                            <label>
+                                {selectedProduct.find(product => product[1] === item.product_id)?.[2] && (
+                                    <textarea
+                                        placeholder="Observação"
+                                        className="w-[200px] mt-1 border border-slate-500 rounded-[5px] p-1"
+                                        onChange={(e) => obsProduct(item.product_id, e.target.value)}
+                                        value={selectedProduct.find(product => product[1] === item.product_id)?.[3] || ""}
+                                        disabled={selectedProduct.findIndex(product => product[1] === item.product_id) === -1}
+                                    />
+                                )}
+                            </label>
 
-                        <div className="flex items-center gap-3 border-2 border-slate-500 rounded-md my-5">
-                            <button className="py-1 px-5 border-r-2 border-slate-500 text-slate-900 hover:text-[#EB8F00] transition-all delay-75"
-                                onClick={() => alterQnt(item.product_id, item.stock, "+")}
-                            ><Plus /></button>
+                            <div className="w-[150px] flex self-end justify-center items-center gap-3 border-2 border-slate-500 rounded-md my-5">
+                                <button className="py-1 px-5 border-r-2 border-slate-500 text-slate-900 hover:text-[#EB8F00] transition-all delay-75"
+                                    onClick={() => alterQnt(item.product_id, item.stock, "+")}
+                                ><Plus /></button>
 
-                            <p className="text-[#EB8F00] font-somibold">
-                                {selectedProduct.find(product => product[1] === item.product_id)?.[2] || 0}
-                            </p>
+                                <p className="text-[#EB8F00] font-somibold">
+                                    {selectedProduct.find(product => product[1] === item.product_id)?.[2] || 0}
+                                </p>
 
-                            <button className="py-1 px-5 border-l-2 border-slate-500 text-slate-900 hover:text-[#EB8F00] transition-all delay-75"
-                                onClick={() => alterQnt(item.product_id, item.stock, "-")}
-                            ><Minus /></button>
+                                <button className="py-1 px-5 border-l-2 border-slate-500 text-slate-900 hover:text-[#EB8F00] transition-all delay-75"
+                                    onClick={() => alterQnt(item.product_id, item.stock, "-")}
+                                ><Minus /></button>
+                            </div>
                         </div>
                     </div>
                 ))}
