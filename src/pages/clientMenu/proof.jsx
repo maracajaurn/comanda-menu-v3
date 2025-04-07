357af03374d8e5409b88d546291c9a26f2ce965f
JackSSads <contatojacksonsouza@hotmail.com>
2025-04-07 18:38:17 -0300

    alterando a cor do hover do botão

--- src/pages/clientMenu/proof.jsx
+++ src/pages/clientMenu/proof.jsx
@@ -135,7 +135,7 @@ export const Proof = () => {
                 <div>
                     <button
                         className="
-                            bg-[#1C1D26] hover:bg-[#EB8F00] hover:text-[#1C1D26] hover:border-[#1C1D26] text-white
+                            bg-[#1C1D26] hover:bg-[#EB8F00] text-white
                             p-2 text-[20px] font-bold rounded-xl border-2 border-transparent  transition-all delay-75"
                         onClick={() => navigate(`/${id}/wait_for_product`)}
                     >Aguardar preparo</button>
