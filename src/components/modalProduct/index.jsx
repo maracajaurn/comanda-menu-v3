483a0a07918206dc52fe2abd7599a0ff53b54ec5
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2025-04-19 12:06:11 -0300

    Corrigindo escrita de height

--- src/components/modalProduct/index.jsx
+++ src/components/modalProduct/index.jsx
@@ -127,7 +127,7 @@ export const ModalProduct = ({ action, id }) => {
                 img.onload = () => {
                     const canvas = document.createElement("canvas");
                     canvas.width = img.width;
-                    canvas.heigth = img.height;
+                    canvas.height = img.height;
 
                     const ctx = canvas.getContext("2d");
                     ctx.drawImage(img, 0, 0);
