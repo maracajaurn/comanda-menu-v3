f154577adb0666e39577dc4c534cdf50581a3eeb
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-01 16:46:55 -0300

    adicionando serviço de categoria de produtos

---
+++ src/service/category/CategoryService.js
@@ -0,0 +1,94 @@
+import { API } from "../axiosConfig";
+
+const getAll = async () => {
+    try {
+        const res = await API.get("/api/category");
+
+        return res.data;
+        return res.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(error.response.data.message || "Erro na resposta da API");
+        } else if (error.request) {
+            throw new Error("Sem resposta do servidor");
+        } else {
+            throw new Error(error.message);
+        }
+    };
+};
+
+const getById = async (id) => {
+    try {
+        const res = await API.get(`/api/category/${id}`);
+
+        return res.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(error.response.data.message || "Erro na resposta da API");
+        } else if (error.request) {
+            throw new Error("Sem resposta do servidor");
+        } else {
+            throw new Error(error.message);
+        }
+    };
+};
+
+const create = async (data) => {
+    try {
+        const res = await API.post("/api/category", data);
+
+        return res.data;
+        return res.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(error.response.data.message || "Erro na resposta da API");
+        } else if (error.request) {
+            throw new Error("Sem resposta do servidor");
+        } else {
+            throw new Error(error.message);
+        }
+    };
+};
+
+const updateById = async (id, data) => {
+    try {
+
+        const res = await API.put(`/api/category/${id}`, data);
+
+        return res.data;
+        return res.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(error.response.data.message || "Erro na resposta da API");
+        } else if (error.request) {
+            throw new Error("Sem resposta do servidor");
+        } else {
+            throw new Error(error.message);
+        }
+    };
+};
+
+const deleteById = async (id) => {
+    try {
+        const res = await API.delete(`/api/category/${id}`);
+
+        return res.data;
+        return res.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(error.response.data.message || "Erro na resposta da API");
+        } else if (error.request) {
+            throw new Error("Sem resposta do servidor");
+        } else {
+            throw new Error(error.message);
+        }
+    };
+};
+
+export const CategoryService = {
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteById
+};
