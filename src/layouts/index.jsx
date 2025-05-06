0ee3ca8820841831512c92bc2c915e1ec0615118
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-06 16:45:22 -0300

    Adicionando o <Toaster /> apenas no layout base

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,6 +1,8 @@
+import { Toaster } from "react-hot-toast";
 export const LayoutBase = ({ children }) => {
     return (
         <div className="min-h-[100dvh] pt-[75px] flex justify-center items-center">
+            <Toaster />
             {children}
         </div>
     );
