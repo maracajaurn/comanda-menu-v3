46cb039931b7e14b15e25ff25cd5743d28f34977
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-06 22:33:22 -0300

    removendo useRCM da raiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
-import { useFCM } from "./hooks/UseFCM";
 
 import { LayoutBase } from "./layouts"
 import { ToggleViewProvider, LoaderContextProvider } from "./contexts";
 
 export const App = () => {
-  useFCM();
-
   return (
     <LoaderContextProvider>
       <BrowserRouter>
