2fe72092df45385e9aa1867d613217203a63c685
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-31 20:56:43 -0300

    exportando novo context ToggleSidebarProvider

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,2 +1,3 @@
 export { LoaderContextProvider, useLoader } from "./LoaderContext";
 export { ToggleViewProvider, useToggleView } from "./ToggleViewNote"
+export { ToggleSidebarProvider, useToggleViewSidebar } from "./ToggleSidebar";
