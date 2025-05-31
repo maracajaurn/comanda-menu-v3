077f0563a62cad86333e9894df3891c453bf7c47
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-31 13:22:15 -0300

    chamando o useFCM

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import { AppRoutes } from "./router";
 import { LayoutBase } from "./layouts"
 import { ToggleViewProvider, LoaderContextProvider } from "./contexts";
 
+import { useFCM } from "./hooks/UseFCM";
+
 export const App = () => {
+
+  useFCM();
+
   return (
     <LoaderContextProvider>
       <BrowserRouter>
