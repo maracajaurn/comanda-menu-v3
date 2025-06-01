1da44944fec67137c1eb1997a2773726dcf07de5
JackSSads <contatojacksonsouza@hotmail.com>
2025-05-31 21:11:32 -0300

    movendo sidebar para o layout base

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,8 +1,21 @@
 import { Toaster } from "react-hot-toast";
+import { Sidebar } from "../components";
+
+const listItems = [
+    { label: "Administração", link: "/admin" },
+    { label: "Produtos", link: "/produtos" },
+    { label: "Configurações", link: "/usuarios" },
+    { label: "Comandas", link: "/garcom/comandas" },
+    { label: "Online", link: "/created_online" },
+    { label: "Cozinha", link: "/cozinha/producao" },
+    { label: "Bar", link: "/barmen/producao" },
+]
+
 export const LayoutBase = ({ children }) => {
     return (
         <div className="min-h-[100dvh] pt-[75px] flex justify-center items-center">
             <Toaster />
+            <Sidebar items={listItems} />
             {children}
         </div>
     );
