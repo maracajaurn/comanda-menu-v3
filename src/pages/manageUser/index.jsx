6a616676a4faa97315cd3c58d975e62e9823a9a4
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-02 14:36:08 -0300

    Corrigindo responsividade

--- src/pages/manageUser/index.jsx
+++ src/pages/manageUser/index.jsx
@@ -1,13 +1,36 @@
-import { ManagerUser, Settings, Categories, Navbar } from "../../components"
+import { useState } from "react";
+
+import { ManagerUser, Settings, Categories, Navbar } from "../../components";
 
 export const ManageUser = () => {
+
+    const [showComponent, setShowComponent] = useState(1);
+
     return (
         <>
             <Navbar title="Configurações" url />
-            <div className="flex flex-col gap-10 mb-10">
-                <ManagerUser />
-                <Settings />
-                <Categories />
+            <div className="p-2 flex flex-col w-full xl:w-[1000px] h-[90dvh] justify-start items-center">
+
+                <div className="w-full flex justify-between items-center gap-2">
+                    <button
+                        className="w-1/3 text-center p-2 border-2 rounded-md border-[#1C1D26] text-[#1C1D26] font-semibold"
+                        onClick={() => setShowComponent(1)}>
+                        Usuários
+                    </button>
+                    <button
+                        className="w-1/3 text-center p-2 border-2 rounded-md border-[#1C1D26] text-[#1C1D26] font-semibold"
+                        onClick={() => setShowComponent(2)}>
+                        Categorias
+                    </button>
+                    <button
+                        className="w-1/3 text-center p-2 border-2 rounded-md border-[#1C1D26] text-[#1C1D26] font-semibold"
+                        onClick={() => setShowComponent(3)}>
+                        Configurações
+                    </button>
+                </div>
+                <ManagerUser showComponent={showComponent} />
+                <Categories showComponent={showComponent} />
+                <Settings showComponent={showComponent} />
             </div>
         </>
     );
