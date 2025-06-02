035fe796c75a8a44263bcef709a947503538e2e1
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-02 19:03:20 -0300

    Corrigindo erro de scroll

--- src/pages/salesHistory/index.jsx
+++ src/pages/salesHistory/index.jsx
@@ -36,7 +36,7 @@ export const SalesHistory = () => {
     return (
         <>
             <Navbar title="Histórico de Vendas" url />
-            <div className="w-full px-5 flex flex-col items-center gap-24 self-start mt-10">
+            <div className="w-full h-full px-5 flex flex-col items-center gap-24 self-start mt-10 overflow-auto">
                 {Object.entries(group).map(([mes, registros]) => (
                     <div key={mes} className="w-full mx-2 overflow-auto sm:w-[500px] md:w-[800px] lg:w-[1000px]">
                         <h2 className="text-xl font-bold mb-2 fixed">Mês: {mes}</h2>
