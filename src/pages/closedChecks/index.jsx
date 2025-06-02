23f5d476402c422d68b7249acb54c9a521442156
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-02 12:45:00 -0300

    Melhorando responsividade

--- src/pages/closedChecks/index.jsx
+++ src/pages/closedChecks/index.jsx
@@ -56,47 +56,48 @@ export const ClosedChecks = () => {
             <Navbar title={`Comandas Fechadas`} url />
 
             <div className="w-[95%] min-h-[90vh] py-3 px-5 rounded-xl flex items-center flex-col gap-5">
-                
-                {rows.length > 0 ? rows.map((e) => (
-                    <div className={` ${e.status ? "hidden" : "flex"}  justify-between items-center my-3 px-5 py-3 w-full rounded-xl bg-slate-100/50 shadow-md`}
-                        key={e.check_id}>
+                <div className="w-full flex sm:flex-row flex-wrap flex-col items-center justify-center gap-5">
+                    {rows.length > 0 ? rows.map((e) => (
+                        <div className="w-full sm:w-[280px] md:w-[350px] xl:w-[550px] flex justify-between items-center my-3 px-5 py-3 rounded-xl bg-slate-100/50 shadow-md"
+                            key={e.check_id}>
 
-                        <div className="flex flex-col">
-                            <h3 className="text-slate-900 font-bold flex flex-col gap-3">
-                                {e.created_for === 1 ? (
-                                    <span className="text-green-500 flex gap-1"><Global /> Online</span>
-                                ) : (
-                                    <span className="text-blue-600 flex gap-1"><Users /> Garçom</span>
-                                )}
-                                <span>{e.name_client}</span>
-                            </h3>
-                            <h3 className="text-slate-400 font-semibold">{e.obs}</h3>
-                            <h4 className="text-slate-500 text-[15px] font-semibold">
-                                <span className="font-bold text-[#EB8F00]">Total:</span> R$ {parseFloat(e.total_value || 0).toFixed(2).replace(".", ",")}</h4>
-                            <p className="text-slate-500 text-[15px] font-semibold flex gap-1">
-                                <span>Pagamento:</span>
-                                <span className="font-bold text-[#EB8F00]">
-                                    {e.pay_form === "credit" ? "Crédito" :
-                                        e.pay_form === "debit" ? "Débito" :
-                                            e.pay_form === "pix" ? "Pix" :
-                                                e.pay_form === "cash" ? "Dinheiro" :
-                                                    "Ainda não foi pago"}
-                                </span>
-                            </p>
+                            <div className="flex flex-col">
+                                <h3 className="text-slate-900 font-bold flex flex-col gap-3">
+                                    {e.created_for === 1 ? (
+                                        <span className="text-green-500 flex gap-1"><Global /> Online</span>
+                                    ) : (
+                                        <span className="text-blue-600 flex gap-1"><Users /> Garçom</span>
+                                    )}
+                                    <span>{e.name_client}</span>
+                                </h3>
+                                <h3 className="text-slate-400 font-semibold">{e.obs}</h3>
+                                <h4 className="text-slate-500 text-[15px] font-semibold">
+                                    <span className="font-bold text-[#EB8F00]">Total:</span> R$ {parseFloat(e.total_value || 0).toFixed(2).replace(".", ",")}</h4>
+                                <p className="text-slate-500 text-[15px] font-semibold flex gap-1">
+                                    <span>Pagamento:</span>
+                                    <span className="font-bold text-[#EB8F00] flex flex-nowrap">
+                                        {e.pay_form === "credit" ? "Crédito" :
+                                            e.pay_form === "debit" ? "Débito" :
+                                                e.pay_form === "pix" ? "Pix" :
+                                                    e.pay_form === "cash" ? "Dinheiro" :
+                                                        "Ainda não foi pago"}
+                                    </span>
+                                </p>
+                            </div>
+
+                            <button className=" p-2 rounded-md bg-[#1C1D26] text-white hover:bg-[#EB8F00] transition-all delay-75"
+                                onClick={() => navigate(`/garcom/comanda/${e.check_id}`)}
+                            ><Plus /></button>
                         </div>
+                    )) : (
+                        <div className="flex justify-between items-center my-3 px-5 py-3 w-full rounded-xl shadow-md">
 
-                        <button className=" p-2 rounded-md bg-[#1C1D26] text-white hover:bg-[#EB8F00] transition-all delay-75"
-                            onClick={() => navigate(`/garcom/comanda/${e.check_id}`)}
-                        ><Plus /></button>
-                    </div>
-                )) : (
-                    <div className="flex justify-between items-center my-3 px-5 py-3 w-full rounded-xl shadow-md">
-
-                        <div className="flex flex-col">
-                            <h3 className="text-slate-900 font-bold">Você não possui comandas finalizadas</h3>
+                            <div className="flex flex-col">
+                                <h3 className="text-slate-900 font-bold">Você não possui comandas finalizadas</h3>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
         </>
     );
