35006d389c4105d03986088ae87b37134db7be3a
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-03 23:30:05 -0300

    Removendo logs

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import { AppRoutes } from "./router";
 import { LayoutBase } from "./layouts"
 import { ToggleViewProvider, LoaderContextProvider, ToggleSidebarProvider } from "./contexts";
 
-import { useFCM } from "./hooks/UseFCM";
-
 export const App = () => {
-  useFCM(null);
-
   return (
     <LoaderContextProvider>
       <BrowserRouter>
