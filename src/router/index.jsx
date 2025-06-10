eb9dbae7ef7c75d12dcadb9b998192b6e3c5c6b1
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-09 22:12:53 -0300

    rotas agrupadas por contexto

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -36,59 +36,34 @@ export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-
-            <Route path="*" element={<NotFound />} />
-            
+            <Route path="/first_access" element={<FirstAccess />} />
             <Route path="/401" element={<NotAuthorized />} />
 
-            <Route path={`/admin`} element={
-                <Admin />
-            } />
-            <Route path={`/sales_history`} element={
-                <SalesHistory />
-            } />
-            <Route path={`/usuarios`} element={
-                <ManageUser />
-            } />
-            <Route path={`/comandasFinalizadas`} element={
-                <ClosedChecks />
-            } />
-            <Route path={`/produtos`} element={
-                <ShowEditProducts />
-            } />
-            <Route path={`/first_access`} element={
-                <FirstAccess />
-            } />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/usuarios" element={<ManageUser />} />
+            <Route path="/sales_history" element={<SalesHistory />} />
+            <Route path="/comandasFinalizadas" element={<ClosedChecks />} />
+            <Route path="/produtos" element={<ShowEditProducts />} />
 
-            <Route path={`/garcom/comanda/:id`} element={
-                <Waiter />
-            } />
-            <Route path={`/:id/garcom/comandas`} element={
-                <ListingChecks />
-            } />
-            <Route path={`/garcom/comanda/:id/add-product`} element={
-                <ListingProducts />
-            } />
-            <Route path={`/garcom/comanda/:id/fechar-comanda`} element={
-                <CloseCheck />
-            } />
+            <Route path="/:id/garcom/comanda" element={<Waiter />} />
+            <Route path="/:id/garcom/comandas" element={<ListingChecks />} />
+            <Route path="/garcom/comanda/:id/add-product" element={<ListingProducts />} />
+            <Route path="/garcom/comanda/:id/fechar-comanda" element={<CloseCheck />} />
 
-            <Route path={`/:id/cozinha/producao`} element={
-                <Cousine />
-            } />
-            <Route path={`/:id/barmen/producao`} element={
-                <Bartender />
-            } />
+            <Route path="/:id/cozinha/producao" element={<Cousine />} />
+            <Route path="/:id/barmen/producao" element={<Bartender />} />
 
-            <Route path={`/register_client`} element={<RegisterClient />} />
-            <Route path={`/:id/products`} element={<Menu />} />
-            <Route path={`/:id/cart`} element={<Cart />} />
-            <Route path={`/:id/to-pay`} element={<ToPay />} />
-            <Route path={`/:id/payment_approved`} element={<PaymentApproved />} />
-            <Route path={`/:id/payment_failure`} element={<PaymentFailure />} />
-            <Route path={`/:id/wait_for_product`} element={<WaitForProducts />} />
+            <Route path="/register_client" element={<RegisterClient />} />
+            <Route path="/:id/products" element={<Menu />} />
+            <Route path="/:id/cart" element={<Cart />} />
+            <Route path="/:id/to-pay" element={<ToPay />} />
+            <Route path="/:id/payment_approved" element={<PaymentApproved />} />
+            <Route path="/:id/payment_failure" element={<PaymentFailure />} />
+            <Route path="/:id/wait_for_product" element={<WaitForProducts />} />
+            <Route path="/:id/created_online" element={<OnlineOrders />} />
 
-            <Route path={`/:id/created_online`} element={<OnlineOrders/>} />
+            <Route path="*" element={<NotFound />} />
+
         </Routes>
     );
 };
