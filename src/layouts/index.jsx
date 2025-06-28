c234c0da08d3936dac46a11893d723f833c94b28
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-28 19:18:18 -0300

    Coprrigindo rota do drawer

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,18 +1,19 @@
 import { Toaster } from "react-hot-toast";
 import { Sidebar } from "../components";
 
-const listItems = [
-    { label: "Administração", link: "/admin" },
-    { label: "Histórico de vendas", link: "/sales_history" },
-    { label: "Comandas", link: "/admin/garcom/comandas" },
-    { label: "Online", link: "/admin/created_online" },
-    { label: "Cozinha", link: "/admin/cozinha/producao" },
-    { label: "Bar", link: "/admin/barmen/producao" },
-    { label: "Produtos", link: "/produtos" },
-    { label: "Configurações", link: "/usuarios" },
-];
-
 export const LayoutBase = ({ children }) => {
+    const user_id = localStorage.getItem("user_id");
+    const listItems = [
+        { label: "Administração", link: `/${user_id}/admin` },
+        { label: "Histórico de vendas", link: "/sales_history" },
+        { label: "Comandas", link: "/admin/garcom/comandas" },
+        { label: "Online", link: "/admin/created_online" },
+        { label: "Cozinha", link: "/admin/cozinha/producao" },
+        { label: "Bar", link: "/admin/barmen/producao" },
+        { label: "Produtos", link: "/produtos" },
+        { label: "Configurações", link: "/usuarios" },
+    ];
+
     return (
         <div className="min-h-[100dvh] pt-[75px] flex justify-center items-center">
             <Toaster />
