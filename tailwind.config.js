e2a793d2966c1e9709c672250aa980d27b08f921
JackSSads <contatojacksonsouza@hotmail.com>
2025-06-29 16:09:12 -0300

    efeito de transição

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeUp: {
+          '0%': { opacity: '0', transform: 'translateY(40px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeUp: 'fadeUp 0.6s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
